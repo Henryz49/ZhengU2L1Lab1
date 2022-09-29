ad37782d75d991c798c01949ed52f4bc06e241a8
BT_3W2_19 <Henryz49@nycstudents.net>
2022-09-29 09:26:28 -0400

    Updated

---
+++ src/CatRunner.java
@@ -0,0 +1,12 @@
+public class CatRunner {
+    public static void main(String[] args){
+        Cat cat1 = new Cat("bob", 3, 40.5)
+        cat1.printCatInfo();
+        cat1.introduce();
+
+        Cat cat2 = new Cat("joe", 4, 20.334)
+        cat2.printCatInfo();
+        cat2.introduce();
+
+    }
+}
